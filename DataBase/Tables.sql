f6eef85ffe0369bf4e8e029572378ea81fae8244
Rafay Raza <154420425+mrafay04@users.noreply.github.com>
2025-08-09 00:31:47 +0500

    Add files via upload

---
+++ DataBase/Tables.sql
@@ -0,0 +1,21 @@
+use CarDB;
+
+CREATE TABLE Owners (
+    OwnerID INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    CNIC NVARCHAR(20) NOT NULL,
+    Contact NVARCHAR(20) NOT NULL
+);
+select *from Owners
+
+CREATE TABLE Vehicle (
+    VehicleID INT PRIMARY KEY IDENTITY,
+    VehicleNumber VARCHAR(20),
+    VehicleType VARCHAR(50),
+    ModelYear INT,
+    EngineNumber VARCHAR(50),
+    ChassisNumber VARCHAR(50),
+    Color VARCHAR(50),
+    OwnerName VARCHAR(100),
+    CNIC VARCHAR(20)
+);
---
+++ DataBase/Vehicle queries.sql
@@ -0,0 +1,63 @@
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('ABC-123', 'Car', 2020, 'EN123456', 'CH123456', 'Black', 'Ali Khan', '42101-1234567-1');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('XYZ-456', 'Bike', 2019, 'EN654321', 'CH654321', 'Red', 'Sara Ahmed', '42201-7654321-2');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('LMN-789', 'Truck', 2021, 'EN987654', 'CH987654', 'Blue', 'Kamran Shah', '42104-1122334-5');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('DEF-321', 'Car', 2022, 'EN321654', 'CH321654', 'White', 'Mehwish Tariq', '42301-9988776-3');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('PQR-654', 'Van', 2018, 'EN456789', 'CH456789', 'Silver', 'Noman Qureshi', '42401-3344556-6');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('JKL-987', 'Bus', 2017, 'EN789123', 'CH789123', 'Yellow', 'Irfan Malik', '42501-2233445-8');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('GHJ-111', 'Car', 2023, 'EN112233', 'CH112233', 'Green', 'Rabia Aslam', '42601-4455667-9');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('TUV-222', 'Bike', 2020, 'EN223344', 'CH223344', 'Black', 'Zeeshan Haider', '42701-5566778-0');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('OPQ-333', 'Car', 2016, 'EN334455', 'CH334455', 'Red', 'Ayesha Jamil', '42801-6677889-1');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('MNO-444', 'Jeep', 2015, 'EN445566', 'CH445566', 'Blue', 'Faraz Ali', '42901-7788990-2');
+
+-- 10 more
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('QWE-555', 'Car', 2021, 'EN556677', 'CH556677', 'Black', 'Hina Naveed', '43001-8899001-3');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('ASD-666', 'Bike', 2014, 'EN667788', 'CH667788', 'Gray', 'Usman Tariq', '43101-9900112-4');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('ZXC-777', 'Truck', 2019, 'EN778899', 'CH778899', 'Orange', 'Shahbaz Khan', '43201-1011121-5');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('VBN-888', 'Bus', 2013, 'EN889900', 'CH889900', 'White', 'Kiran Saeed', '43301-2122232-6');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('RTY-999', 'Car', 2024, 'EN990011', 'CH990011', 'Golden', 'Bilal Qazi', '43401-3233343-7');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('UYT-101', 'Van', 2016, 'EN101112', 'CH101112', 'Maroon', 'Fahad Ansari', '43501-4344454-8');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('NBV-202', 'Car', 2018, 'EN112223', 'CH112223', 'Black', 'Hira Naseem', '43601-5455565-9');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('WSX-303', 'Bike', 2017, 'EN223334', 'CH223334', 'Red', 'Tariq Hussain', '43701-6566676-0');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('EDC-404', 'Car', 2015, 'EN334445', 'CH334445', 'White', 'Sameera Iqbal', '43801-7677787-1');
+
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('RFV-505', 'Bus', 2022, 'EN445556', 'CH445556', 'Blue', 'Adnan Rauf', '43901-8788898-2')
+INSERT INTO Vehicle (VehicleNumber, VehicleType, ModelYear, EngineNumber, ChassisNumber, Color, OwnerName, CNIC)
+VALUES ('AXY-461', 'Saloon', 2012, 'EN889900', 'CH889900', 'White', 'Rafay Raza ', '42101-9833637-5');
